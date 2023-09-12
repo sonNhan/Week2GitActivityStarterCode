b14d06fa5324eb56d2c2e429050d0b00c3651b3d
NextGenSledder <jmichel.peterson@gmail.com>
2023-09-12 15:02:42 -0400

    added subtraction op

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
+const result_sub = calc.subtract(2, 3);
+console.log(`result_sub = ${result_sub}`);
 
