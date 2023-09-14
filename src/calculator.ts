25c1993db3d8d8cb3d39a7dbaa6066a255e1a4c1
parsley <nhanson@posteo.net>
2023-09-14 00:00:03 -0400

    print hello message

--- src/calculator.ts
+++ src/calculator.ts
@@ -8,4 +8,7 @@ export class Calculator {
   public multiply(x: number, y: number) : number {
     return x * y;
   }
+  public hello() {
+    console.log("hello");
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 const result_sub = calc.subtract(2, 3);
 console.log(`result_sub = ${result_sub}`);
+console.log(calc.hello);
 
